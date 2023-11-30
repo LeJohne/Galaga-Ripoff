cbb160a5cacd87e7e53b43f0949326587ae5722e
Matt <mhendsch@bu.edu>
2023-11-30 13:08:13 -0500

    Test1 of bullet and enemy
    Added bullet and enemy

--- app/src/main/java/com/example/galagaremake/gamescreen_page.java
+++ app/src/main/java/com/example/galagaremake/gamescreen_page.java
@@ -1,6 +1,8 @@
 package com.example.galagaremake;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -16,21 +18,38 @@ public class gamescreen_page extends AppCompatActivity {
     private SensorManager sensorManager;
     private Sensor rotationVectorSensor;
     private SensorEventListener rotationListener;
-    private ImageView shipImage;
+    public ImageView shipImage;
 
+    public ImageView bulletImage;
     private TextView rightText;
 
     private TextView leftText;
-    private int shipX;
+    public int shipX;
     private int shipY;
     private int score;
+
+
     private boolean startFlag = false;
     private Rect shipCollisionBox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamescreen_page);
+
+        findViewById(R.id.gameScreenLayout).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                bullet bullet = new bullet((ImageView) findViewById(R.id.bullet));
+
+                while (bullet.bulletY < 1000) {
+                    bullet.move();
+                }
+
+
+            }
+        });
 
         // Initialize shipImage after setting the content view
         shipImage = findViewById(R.id.ship);
@@ -73,10 +92,14 @@ public class gamescreen_page extends AppCompatActivity {
                     }
                 }
 
+
+
                 @Override
                 public void onAccuracyChanged(Sensor sensor, int i) {
                     // Do something if accuracy changes
                 }
+
+
             };
         }
     }
@@ -96,6 +119,70 @@ public class gamescreen_page extends AppCompatActivity {
             sensorManager.unregisterListener(rotationListener);
         }
     }
+
+
+
+  //create a bullet class that will create a new imageview
+    //and move it up the screen until it hits the top of the screen
+    //or hits an enemy
+    //if it hits an enemy, remove the enemy and the bullet
+    //if it hits the top of the screen, remove the bullet
+    //if it hits nothing, keep moving it up the screen
+    //create a new bullet when the user taps the screen
+
+    public class bullet {
+        private int bulletX;
+        private int bulletY;
+        private ImageView bulletImage;
+        private Rect bulletCollisionBox;
+
+        public bullet(ImageView bulletImage) {
+            this.bulletImage = bulletImage;
+            bulletX = (int)shipImage.getX() + (shipImage.getWidth() / 2);
+            bulletY = (int)shipImage.getY() - shipImage.getHeight();
+            bulletImage.setX(bulletX);
+            bulletImage.setY(bulletY);
+            bulletImage.setVisibility(View.VISIBLE);
+            bulletCollisionBox = new Rect(bulletImage.getWidth(), bulletImage.getHeight(), bulletImage.getWidth(), bulletImage.getHeight());
+            bulletCollisionBox.set((int) bulletImage.getX(), (int)bulletImage.getY(), (int)bulletImage.getX() + bulletImage.getWidth(), (int)bulletImage.getY() - bulletImage.getHeight());
+        }
+
+        public void move() {
+            bulletY = bulletY + 10;
+            bulletImage.setY(bulletY);
+            bulletCollisionBox.set((int) bulletImage.getX(), (int)bulletImage.getY(), (int)bulletImage.getX() + bulletImage.getWidth(), (int)bulletImage.getY() - bulletImage.getHeight());
+        }
+    }
+
+    public class enemy {
+        private int enemyX;
+        private int enemyY;
+        private ImageView enemyImage;
+        private Rect enemyCollisionBox;
+
+        public enemy(ImageView enemyImage) {
+            this.enemyImage = enemyImage;
+            enemyX = (int)enemyImage.getX();
+            enemyY = (int)enemyImage.getY();
+            enemyImage.setX(enemyX);
+            enemyImage.setY(enemyY);
+            enemyImage.setVisibility(View.VISIBLE);
+            enemyCollisionBox = new Rect(enemyImage.getWidth(), enemyImage.getHeight(), enemyImage.getWidth(), enemyImage.getHeight());
+            enemyCollisionBox.set((int) enemyImage.getX(), (int)enemyImage.getY(), (int)enemyImage.getX() + enemyImage.getWidth(), (int)enemyImage.getY() - enemyImage.getHeight());
+        }
+
+        public void move() {
+            enemyY = enemyY + 10;
+            enemyImage.setY(enemyY);
+            enemyCollisionBox.set((int) enemyImage.getX(), (int)enemyImage.getY(), (int)enemyImage.getX() + enemyImage.getWidth(), (int)enemyImage.getY() - enemyImage.getHeight());
+        }
+
+        public void remove() {
+            enemyImage.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
 }
 
 
